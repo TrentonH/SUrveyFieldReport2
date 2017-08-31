d7d61a02204c85785e26bda4f91f375e95cf3098
Trenton Hamilton <trentonhamilton@trentons-mbp.hsd1.co.comcast.net>
2017-08-31 13:07:39 -0600

    created connections to all of the elements in the app and created a outline for the reset button

--- SurveyFieldReport2/ViewController.swift
+++ SurveyFieldReport2/ViewController.swift
@@ -9,11 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var SubmitB: UIButton!
+    @IBOutlet weak var DateB: UIDatePicker!
+    @IBOutlet weak var ResetB: UIButton!
+    @IBOutlet weak var LotI: UITextField!
+    @IBOutlet weak var BlockI: UITextField!
+    @IBOutlet weak var EmailI: UITextField!
+    @IBOutlet weak var BuilderI: UITextField!
+    @IBOutlet weak var SubdivisionI: UITextField!
+    @IBOutlet weak var FilingI: UITextField!
+    @IBOutlet weak var AddressI: UITextField!
+    @IBOutlet weak var HSSPPP: UISwitch!
+    @IBOutlet weak var HSUTSLNC: UISwitch!
+    @IBOutlet weak var HSOSC: UISwitch!
+    @IBOutlet weak var FFCPPPP: UISwitch!
+    @IBOutlet weak var FFCFSC: UISwitch!
+    @IBOutlet weak var FFCOSC: UISwitch!
+    @IBOutlet weak var RGSSPPP: UISwitch!
+    @IBOutlet weak var RGSOSC: UISwitch!
+    @IBOutlet weak var ISSSFR: UISwitch!
+    @IBOutlet weak var ISFNC: UISwitch!
+    @IBOutlet weak var ISOSC: UISwitch!
+    @IBOutlet weak var GSPSSFG: UISwitch!
+    @IBOutlet weak var GSFGF: UISwitch!
+    @IBOutlet weak var GSFDNMC: UISwitch!
+    @IBOutlet weak var GSOSC: UISwitch!
+    @IBOutlet weak var CommentsI: UITextView!
+    
+    
+    @IBAction func ResetBA(_ sender: Any) {
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
